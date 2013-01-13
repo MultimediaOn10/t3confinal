a39ce04242b267d0f975c3ffe8ef8ae6c89e2e42
Liesa <liesa@burgey-online.com>
2013-01-13 20:57:59 +0100

    add encoding lastname

--- php/ajaxrequest_nametitle.php
+++ php/ajaxrequest_nametitle.php
@@ -10,11 +10,10 @@ $trackID = $_POST ["trackID"];
 //Ausgabe für den text
 	$query = "Select programpoints.title, programpoints.id, ref1.lastname as ref1_last, ref1.firstname as ref1_first, ref2.lastname as ref2_last, ref2.firstname as ref2_first from programpoints left join (referents as ref1) on (programpoints.`referent_id`= ref1.`id`) left join (referents as ref2) on (programpoints.`referent2_id`= ref2.`id`) where programpoints.`id`='$trackID'";
     $getspeakertitle = mysql_query($query); 
-	
     $rows = array();
         
     while($row = mysql_fetch_array($getspeakertitle)) {
-	
+	$row['ref1_last']=utf8_encode($row['ref1_last']);
     $rows[] = $row;
 	}
     echo json_encode(array("items" => $rows));
