6b14cb1a881f0f958a29b9e6cbee301256ea0b2f
Liesa <liesa@burgey-online.com>
2013-01-13 13:45:46 +0100

    add database query to ajaxrequest_nametitle

--- php/ajaxrequest_nametitle.php
+++ php/ajaxrequest_nametitle.php
@@ -1,13 +1,14 @@
 <?php
 //Variable
 $trackID = $_POST ["trackID"];
+
 //Verbindung zur Datenbank  
     include ('databaseconfig.php'); 
 	$connection=mysql_connect($mysqlhost, $mysqluser, $mysqlpwd);
 	mysql_select_db($mysqldb, $connection);	
     
 //Ausgabe für den text
-	$query = "Select * from programpoints where `programpoint_id`='$trackID'";
+	$query = "Select programpoints.title, programpoints.id, ref1.lastname as ref1_last, ref1.firstname as ref1_first, ref2.lastname as ref2_last, ref2.firstname as ref2_first from programpoints left join (referents as ref1) on (programpoints.`referent_id`= ref1.`id`) left join (referents as ref2) on (programpoints.`referent2_id`= ref2.`id`) where programpoints.`id`='$trackID'";
     $getspeakertitle = mysql_query($query); 
 	
     $rows = array();
@@ -15,7 +16,7 @@ $trackID = $_POST ["trackID"];
     while($row = mysql_fetch_array($getspeakertitle)) {
 	
     $rows[] = $row;
-    }	
+	}
     echo json_encode(array("items" => $rows));
     
 ?>
