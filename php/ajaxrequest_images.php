842a6cc86fbe4ba25e202a531458dceaf5c3b032
Respace <lamafaun@web.de>
2013-01-16 15:34:06 +0100

    commit that file, so i can pull

--- php/ajaxrequest_images.php
+++ php/ajaxrequest_images.php
@@ -6,26 +6,12 @@ foreach ($alledateien as $datei) { // Ausgabeschleife
     //echo $datei."<br />"; //Ausgabe Einzeldatei
 		if (strpos($datei, "thumb")==false){
 			$datei_blanko=str_replace(".jpg","",$datei);
-			echo ("<div class='slides_container'>
-						<div class='slide'>
-							<div class='item'>
-								<a class='fancybox-images' data-thumbnail='../img/break1/".$datei_blanko."\"\"_thumb.jpg' href='../img/break1/".$datei_blanko.".jpg'>".$datei."</a>
-							</div>
-						</div>
-					</div>");
+			echo ("<div class='item'>
+								<a class='fancybox-images' data-thumbnail='../img/break1/".$datei_blanko."_thumb.jpg' href='../img/break1/".$datei_blanko.".jpg'>".$datei."</a>
+					</div>
+						");
 		}
 
 	
 	};
 ?>
-
-<!--
-
-<div class='slides_container'>
-	<div class='slide'>
-		<div class='item'>
-			<a class='fancybox-images' data-thumbnail='../img/break1/MG_8605""_thumb.jpg' href='../img/break1/MG_8605.jpg'>Open #2</a>
-		</div>
-	</div>
-</div>
--->
