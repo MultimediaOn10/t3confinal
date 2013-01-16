c9745719f5a88f56cac53d2156ae2b550dc5ff66
Liesa <liesa@burgey-online.com>
2013-01-16 15:06:19 +0100

    berordner wird nicht ausgelesen

--- php/ajaxrequest_images.php
+++ php/ajaxrequest_images.php
@@ -2,21 +2,25 @@
 $breakname = "break1";
 
 $alledateien = scandir('../img/'.$breakname); //Ordner "files" auslesen
+$img_array=array();
 foreach ($alledateien as $datei) { // Ausgabeschleife
     //echo $datei."<br />"; //Ausgabe Einzeldatei
-		if (strpos($datei, "thumb")==false){
+	
+	
+		if (strpos($datei, "thumb")==false && $datei != "." && $datei != ".."){
 			$datei_blanko=str_replace(".jpg","",$datei);
-			echo ("<div class='slides_container'>
+			$img="<div class='slides_container'>
 						<div class='slide'>
 							<div class='item'>
 								<a class='fancybox-images' data-thumbnail='../img/break1/".$datei_blanko."\"\"_thumb.jpg' href='../img/break1/".$datei_blanko.".jpg'>".$datei."</a>
 							</div>
 						</div>
-					</div>");
-		}
-
+					</div>";
+		$img_array[]=$img;
+		}		
 	
 	};
+	print_r($img_array);
 ?>
 
 <!--
