8ba66a7f028d09edbbed632cbc571a6a65c2bd9c
Liesa <liesa@burgey-online.com>
2013-01-16 13:14:33 +0100

    phpabfrage bilder

--- php/ajaxrequest_images.php
+++ php/ajaxrequest_images.php
@@ -3,9 +3,23 @@ $breakname = "break1";
 
 $alledateien = scandir('../img/'.$breakname); //Ordner "files" auslesen
 foreach ($alledateien as $datei) { // Ausgabeschleife
-    echo $datei."<br />"; //Ausgabe Einzeldatei
-};
+    //echo $datei."<br />"; //Ausgabe Einzeldatei
+		if (strpos($datei, "thumb")==false){
+			$datei_blanko=str_replace(".jpg","",$datei);
+			echo ("<div class='slides_container'>
+						<div class='slide'>
+							<div class='item'>
+								<a class='fancybox-images' data-thumbnail='../img/break1/".$datei_blanko."\"\"_thumb.jpg' href='../img/break1/".$datei_blanko.".jpg'>".$datei."</a>
+							</div>
+						</div>
+					</div>");
+		}
+
+	
+	};
 ?>
+
+<!--
 
 <div class='slides_container'>
 	<div class='slide'>
@@ -14,3 +28,4 @@ foreach ($alledateien as $datei) { // Ausgabeschleife
 		</div>
 	</div>
 </div>
+-->
